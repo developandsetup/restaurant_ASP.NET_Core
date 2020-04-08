ca0970ea6e0417ff1b77eb15ab709a7e51cab860
Danko Papucic <dpapucic@hrsinternational.com>
2020-04-08 17:38:55 +0200

    Add project files.

---
+++ OdeToFood.Web/Controllers/HomeController.vb
@@ -0,0 +1,23 @@
+﻿Using System.Web;
+
+Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Public String Index()
+        {
+        Return "Hello, World!";
+        'Return View()
+        }
+
+    Function About() As ActionResult
+        ViewData("Message") = "Your application description page."
+
+        Return View()
+    End Function
+
+    Function Contact() As ActionResult
+        ViewData("Message") = "Your contact page."
+
+        Return View()
+    End Function
+End Class
